0d04c989bffd297a2500455a8f7f4d702d0aeef3
abrosimov <abrosimov@tdme.ru>
2023-10-13 14:28:26 +0300

    lintfix

--- components/header/MHeaderSearch.vue
+++ components/header/MHeaderSearch.vue
@@ -4,17 +4,17 @@
   >
     <div class="relative" style="z-index: 99999">
       <nuxt-link
-          v-for="shop of shops"
-          :key="shop.uin"
-          :to="`/shop/${shop.lat_title}`"
-          class="flex items-center px-3 py-2 border-b-2 dark:border-zinc-700 hover:bg-gray-100 dark:hover:bg-zinc-700"
+        v-for="shop of shops"
+        :key="shop.uin"
+        :to="`/shop/${shop.lat_title}`"
+        class="flex items-center px-3 py-2 border-b-2 dark:border-zinc-700 hover:bg-gray-100 dark:hover:bg-zinc-700"
       >
         <img
-            :title="shop.title"
-            loading="lazy"
-            :src="`https://za-halyavoi.ru/api/static/${shop.image}`"
-            :alt="shop.image"
-            class="mr-2 h-10 xl:h-16 w-auto"
+          :title="shop.title"
+          loading="lazy"
+          :src="`https://za-halyavoi.ru/api/static/${shop.image}`"
+          :alt="shop.image"
+          class="mr-2 h-10 xl:h-16 w-auto"
         />
         <div class="uppercase">{{ shop.title }}</div>
       </nuxt-link>
